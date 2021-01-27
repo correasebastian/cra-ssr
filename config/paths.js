00c7e5037b4bdc5028a88360c75ade5454cd4fb3
sebastian correa <sebastian@sawyereffect.com>
2021-01-27 11:25:44 -0500

    add path

--- config/paths.js
+++ config/paths.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const path = require('path');
 const fs = require('fs');
@@ -56,6 +56,7 @@ module.exports = {
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
+  appServerIndexJs: resolveModule(resolveApp, 'server/index'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
   appTsConfig: resolveApp('tsconfig.json'),
