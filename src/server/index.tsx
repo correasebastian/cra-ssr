1975d39fd2bde267537684c7cbe3e5ebb348ddae
sebastian correa <sebastian@sawyereffect.com>
2021-02-03 19:00:33 -0500

    using express ok

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -10,17 +10,15 @@ const renderK = (extractor: any, location?: string) =>{
     console.log("🚀 ------------------------------------------------------------")
     console.log("🚀 ~ file: index.tsx ~ line 10 ~ renderK ~ reactDom", reactDom)
     console.log("🚀 ------------------------------------------------------------")
-    
+        // const scriptTags = extractor.getScriptTags() // or extractor.getScriptElements();
 
-        const scriptTags = extractor.getScriptTags() // or extractor.getScriptElements();
+        // // You can also collect your "preload/prefetch" links
+        // const linkTags = extractor.getLinkTags() // or extractor.getLinkElements();
 
-        // You can also collect your "preload/prefetch" links
-        const linkTags = extractor.getLinkTags() // or extractor.getLinkElements();
+        // // And you can even collect your style tags (if you use "mini-css-extract-plugin")
+        // const styleTags = extractor.getStyleTags() // or extractor.getStyleElements();
 
-        // And you can even collect your style tags (if you use "mini-css-extract-plugin")
-        const styleTags = extractor.getStyleTags() // or extractor.getStyleElements();
-
-
+    return reactDom
 }
 
 
