e467e7089740182f93ead44735cdb087e741dc53
sebastian correa <sebastian@sawyereffect.com>
2021-02-05 11:19:13 -0500

    react-helmet clientside

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,10 +2,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import Sebas from 'component-sebas'
+import {Helmet} from "react-helmet";
+
 
 const Home =() =>{
     return (
     <div className="App">
+        <Helmet>
+            <title>Home title</title>
+        </Helmet>
         <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
