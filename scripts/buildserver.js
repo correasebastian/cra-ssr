86660236df93d371d969d8ff1722a8e64bec635f
sebastian correa <sebastian@sawyereffect.com>
2021-02-12 16:49:45 -0500

    naming fixed

--- scripts/buildserver.js
+++ scripts/buildserver.js
@@ -3,9 +3,8 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'server';
 process.env.NODE_ENV = 'server';
-const isWatchMode = process.env.WATCHSERVER==="TRUE"
+const isWatchMode = process.env.WATCH_SERVER==="TRUE"
 
-console.log(process.env.WATCHSERVER)
 console.log('isWatchMode', isWatchMode)
 
 // Makes the script crash on unhandled rejections instead of silently
