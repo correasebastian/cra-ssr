a2ba2cb78903636b54648493a99532609443625c
sebastian correa <sebastian@sawyereffect.com>
2021-02-18 14:44:07 -0500

    using functions inside makestyles

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,24 +3,40 @@ import React from 'react';
 import logo from './logo.svg';
 import Sebas from 'component-sebas'
 import {Helmet} from "react-helmet";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 
 
-const useStyles = makeStyles({
-    root: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      border: 0,
-      borderRadius: 3,
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      color: 'white',
-      height: 48,
-      padding: '0 30px',
-      width:'200px'
-    },
-    imageWrapper: props => props
-  });
+// const useStyles = makeStyles({
+//     root: {
+//       background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+//       border: 0,
+//       borderRadius: 3,
+//       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+//       color: 'white',
+//       height: 48,
+//       padding: '0 30px',
+//       width:'200px'
+//     },
+//     imageWrapper: props => props
+//   });
+
+  const useStyles = makeStyles(
+    createStyles({
+        root: {
+            background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+            border: 0,
+            borderRadius: 3,
+            boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+            color: 'white',
+            height: 48,
+            padding: '0 30px',
+            width:'200px'
+        },
+      imageWrapper: props => props
+    })
+  );
 
 const Home =() =>{
     const classes = useStyles({backgroundColor:"red"});
